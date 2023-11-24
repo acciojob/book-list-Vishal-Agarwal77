354f43f9a8a94120b1cc468c8a475058d12fde67
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-24 20:48:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const form=document.getElementsByClassName("form-group")[0];
+const title=document.getElementById("title");
+const author=document.getElementById("author");
+const isbn=document.getElementById("isbn");
+form.addEvent
