e0dc9bf7952b3d72fe6b9fbb68608bf7e663003c
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-24 22:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ const form=document.getElementsByClassName("form-group")[0];
 const title=document.getElementById("title");
 const author=document.getElementById("author");
 const isbn=document.getElementById("isbn");
-form.addEvent
+const table_body=document.getElementById("book-list");
+form.addEventListener("submit",()=>{
+	let row=document.createElement("tr");
+	let col1=document.createElement("td");
+	col1.innerText=`${title.value}`;
+	row.appendChild(col1);
+	let col2=document.createElement("td");
+	col2.innerText=`${author.value}`;
+	row.appendChild(col2);
+	let col3=document.createElement("td");
+	col3.innerText=`${isbn.value}`;
+	row.appendChild(col3);
+	console.log(row.innerHTML);
+	table_body.appendChild(row);
+	console.log(table_body);
+})
