7f64662540ba9dab61c453adf27c01d9fa68a278
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-25 00:48:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,18 +4,31 @@ const title=document.getElementById("title");
 const author=document.getElementById("author");
 const isbn=document.getElementById("isbn");
 const table_body=document.getElementById("book-list");
-form.addEventListener("submit",()=>{
+form.addEventListener("submit",(event)=>{
+	event.preventDefault();
 	let row=document.createElement("tr");
 	let col1=document.createElement("td");
-	col1.innerText=`${title.value}`;
-	row.appendChild(col1);
 	let col2=document.createElement("td");
-	col2.innerText=`${author.value}`;
-	row.appendChild(col2);
 	let col3=document.createElement("td");
+	let col4=document.createElement("td");
+	let btn=document.createElement("button");
+	col1.innerText=`${title.value}`;
+	col2.innerText=`${author.value}`;
 	col3.innerText=`${isbn.value}`;
+	btn.innerHTML=`<i class="fa-solid fa-trash"></i>`;
+	btn.className="delete";
+	// console.log(btn);
+	col4.appendChild(btn);
+	row.appendChild(col1);
+	row.appendChild(col2);
 	row.appendChild(col3);
-	console.log(row.innerHTML);
+	row.appendChild(col4);
 	table_body.appendChild(row);
-	console.log(table_body);
+	let del=document.getElementsByTagName("i");
+	for(let item of del){
+		item.addEventListener("click",()=>{
+			let node=item.parentNode.parentNode;
+			node.remove();
+		})	
+	}
 })
